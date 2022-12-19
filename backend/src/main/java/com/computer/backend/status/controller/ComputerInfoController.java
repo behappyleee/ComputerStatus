eb368a237730e75c36cfb410a68a8573e4d02943
leechulhan <dhfhfkwhdk@naver.com>
2022-12-19 23:56:02 +0900

    Computer Project Commit

--- backend/src/main/java/com/computer/backend/status/controller/ComputerInfoController.java
+++ backend/src/main/java/com/computer/backend/status/controller/ComputerInfoController.java
@@ -1,14 +1,12 @@
 package com.computer.backend.status.controller;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalTime;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -27,33 +25,25 @@ public class ComputerInfoController {
 	@Autowired
 	ComputerInfoService computerInfoService;
 	
-	@GetMapping("/computerBasicData")
+	@RequestMapping("/computerBasicData")
 	@ResponseBody
 	public ComputerBasicDTO coumputerBasicData(HttpServletRequest request) {
 		logger.debug("computerBasinData REQ : {} ", request);
+		logger.debug("RETURN DATA CHECK : {} ", computerInfoService.ComputerInfo(request));
+		
 		return computerInfoService.ComputerInfo(request);
 	}
 	
-	public static void main(String[] args) {
-//		Properties test = System.getProperties();
-//		for(Object obj : test.keySet()) {
-//			logger.debug("OBJECT DATA : {}  VALUE : {} " , obj, test.get(obj));
-//		}
-		logger.trace("LOG TRACE TEST !");
-		logger.debug("LOG DEBUG TEST !");	
-		logger.info("LOG INFO TEST");
-		logger.warn("LOG WARN TEST!");
-		logger.error("LOG ERROR TEST !");
-		
-		
-		List <String> listA = new ArrayList <String>();
-		
-		listA.add("1");
-		listA.add("2");
-		listA.add("3");
-		
-		logger.debug("LIST ARRAY CHECK : {} ");
-		
+	@RequestMapping("/getTimeFromBack")
+	@ResponseBody
+	public LocalTime localTime() {
+		return LocalTime.now();
 	}
+	
+	public static void main(String[] args) {
+		System.out.println("JAVA LOCAL DATA : " + LocalTime.now());
+	}
+	
+	
 	
 }
--- frontend/src/main/Main.tsx
+++ frontend/src/main/Main.tsx
@@ -1,17 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 function Main() {
+    const [os, setOs] = useState(''); 
+    const [cpu, setCpu] = useState('');
+    const [userCountry, setUserCountry] = useState('');
+
     useEffect(() => {
-        fetch("http://localhost:8080/computerInfo/v1/computerBasicData")
+        axios.get("http://localhost:8080/computerInfo/v1/computerBasicData")
             .then((res) => {
-
-                console.log('RES DATA CHECK : ' + JSON.stringify(res));
-
-            }).catch((err) => {
-            console.log('computerBasicData ERR : ' + err);
+                let basicComputerData = res.data;
+                setOs(basicComputerData.os);
+                setCpu(basicComputerData.cpu);
+                setUserCountry(basicComputerData.userCountry);
             })
+
+        axios.get("http://localhost:8080/getTimeFromBack")
+            .then((res) => {
+                console.log('겟 알이에스 : ' + res);
+            })
+
     }, [])
+
+    useEffect(() => {
+
+    }, [os, cpu, userCountry])
     
+    async function getDataFromBack() {
+        axios({
+            url : 'http://localhost:8080/computerInfo/v1/computerBasicData',
+            method: 'GET',
+        }).then((res) => {
+
+            console.log('GET DATA FROM BACL : ' + JSON.stringify(res));
+        
+        }).catch((err) => {
+            console.log('getDataFromBack ERR : ' + err);
+        })
+    }
+
+
     // Spring backend 에서 가져온 CPU 점유율 데이터 표출 해주기 .....
     // function cpuStatusData() {
     // }
@@ -20,12 +49,13 @@ function Main() {
         <div>
             <h1>Computer Status Main Page</h1>
             <div>
-                <h2> 사용 OS : </h2>
-                {/* <p> </p> */}
+                <h2> 사용 OS : {os}</h2>
             </div>
             <div>
-                <h2>CPU STATUS</h2>    
-                {/* <p>CPU USAGE : </p> */}
+                <h2>CPU 종류 : {cpu}</h2>    
+            </div>
+            <div>
+                <h2>접속 국가 : {userCountry}</h2>    
             </div>
         </div>
     )
